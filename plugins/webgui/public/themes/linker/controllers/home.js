fa2a3ab60cdf4061ef2def911e3b8bbe90a5b326
lokisama <249722824@qq.com>
2017-06-18 03:19:51 +0800

    x

--- plugins/webgui/public/themes/linker/controllers/home.js
+++ plugins/webgui/public/themes/linker/controllers/home.js
@@ -41,22 +41,43 @@ app
   ])
   .controller('HomeIndexController', ['$scope', '$state',
     ($scope, $state) => {
-      $scope.icons = [{
-        icon: 'flash_on',
-        title: '快速搭建',
-        content: '仅依赖Node.js，无需安装数据库（可选MySQL）',
-      }, {
-        icon: 'build',
-        title: '易于配置',
-        content: '带有插件系统，仅需修改配置文件即可运行',
-      }, {
-        icon: 'vpn_key',
-        title: '官方标准',
-        content: '支持libev和python版本的标准manager API',
-      }];
+
       $scope.login = () => { $state.go('home.login'); };
       $scope.signup = () => { $state.go('home.signup'); };
+   
+      $scope.user = {};
+      $scope.sendCodeTime = 0;
+      $scope.sendCode = () => {
+        alertDialog.loading();
+        homeApi.sendCode($scope.user.email)
+        .then(success => {
+          alertDialog.show('验证码已发至邮箱', '确定');
+          $scope.sendCodeTime = 120;
+          const interval = $interval(() => {
+            if ($scope.sendCodeTime > 0) {
+              $scope.sendCodeTime--;
+            } else {
+              $interval.cancel(interval);
+              $scope.sendCodeTime = 0;
+            }
+          }, 1000);
+        }).catch(err => {
+          alertDialog.show(err, '确定');
+        });
+      };
+      $scope.signup = () => {
+        alertDialog.loading();
+        homeApi.userSignup($scope.user.email, $scope.user.code, $scope.user.password).then(success => {
+          alertDialog.show('用户注册成功', '确定').then(success => {
+            $state.go('home.login');
+          });
+        }).catch(err => {
+          alertDialog.show(err, '确定');
+        });
+      };
+
     }
+
   ])
   .controller('HomeLoginController', ['$scope', '$state', 'homeApi', 'alertDialog', '$localStorage',
     ($scope, $state, homeApi, alertDialog, $localStorage) => {
