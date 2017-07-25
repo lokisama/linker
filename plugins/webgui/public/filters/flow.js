f3cc8fa399e4a180f727b24cafb54d8110f2565e
lokisama <249722824@qq.com>
2017-07-25 14:14:19 +0800

    flow index

--- plugins/webgui/public/filters/flow.js
+++ plugins/webgui/public/filters/flow.js
@@ -7,6 +7,7 @@ app.filter('flow', function() {
   const T = 1000 * 1000 * 1000 * 1000;
   const P = 1000 * 1000 * 1000 * 1000 * 1000;
   return function(input) {
+
     if (input < K) {
       return input + ' B';
     } else if (input < M) {
@@ -14,11 +15,7 @@ app.filter('flow', function() {
     } else if (input < G) {
       return (input / M).toFixed(1) + ' MB';
     } else if (input < T) {
-      if((input / G).toFixed(2) < 50){
         return (input / G).toFixed(2) + ' GB';
-      }else{
-        return '无限流量';
-      }
     } else if (input < P) {
       return (input / T).toFixed(3) + ' TB';
     } else {
@@ -26,3 +23,36 @@ app.filter('flow', function() {
     }
   };
 });
+
+app.filter('flowServer', function() {
+  const K = 1000;
+  const M = 1000 * 1000;
+  const G = 1000 * 1000 * 1000;
+  const T = 1000 * 1000 * 1000 * 1000;
+  const P = 1000 * 1000 * 1000 * 1000 * 1000;
+  return function(input) {
+    return '无限流量';
+
+    
+
+    if (input < K) {
+      return input + ' B';
+    } else if (input < M) {
+      return (input / K).toFixed(1) + ' KB';
+    } else if (input < G) {
+      return (input / M).toFixed(1) + ' MB';
+    } else if (input < T) {
+      if((input / G) < 50){
+        return (input / G).toFixed(2) + ' 无限流量';
+      }else{
+        return '无限流量';
+      }
+
+    } else if (input < P) {
+      return (input / T).toFixed(3) + ' TB';
+    } else {
+      return input;
+    }
+  };
+});
+
